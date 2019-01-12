669a77739895c3d61f7e660b0bde3eb8a5aff1ac
lensonzhu <136453029@qq.com>
2019-01-12 23:04:27 +0800

    8d:done login out register

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,40 @@
+from flask import render_template,redirect,url_for,flash
+from . import auth
+from .. models import User
+from . forms import LoginForm,RegisterForm
+from flask_login import login_user,logout_user,login_required,current_user
+from .. import db
+from .. email import send_email
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember_me.data)
+            print('username======',user)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+        print('username////////',user)
+    return render_template('auth/login.html',form=form)
 
 
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logouting. ')
+    return redirect(url_for('main.index'))
 
+@auth.route('/register',methods=['GET','POST'])
 def register():
-    pass
+    form=RegisterForm()
+    if form.validate_on_submit():
+        user=User(email=form.email.data,password=form.password.data,username=form.username.data)
+        db.session.add(user)
+        flash('You can now login')
+        print('user======',user)
+        return redirect(url_for('auth.login'))
+    print('user-------',form)
+    return render_template('auth/register.html',form=form)
--- app/email.py
+++ app/email.py
@@ -5,11 +5,11 @@ from . import mail
 
 def send_async_email(app,msg):
     with app.app_context():
-        mail.semd(msg)
+        mail.send(msg)
 
 
 def send_email(to,subject,template,**kwargs):
-    app=current_app.get_current_object()
+    app=current_app._get_current_object()
     msg=Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+''+subject,sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
     msg.html=render_template(template+'.html',**kwargs)
     msg.body=render_template(template+'.txt',**kwargs)
